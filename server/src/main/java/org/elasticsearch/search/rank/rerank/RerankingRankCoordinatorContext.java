b26b0ce3a011e9c1a427d88349676521b940e638
Panagiotis Bailis <pmpailis@gmail.com>
2024-03-28 16:06:41 +0200

    iter

--- server/src/main/java/org/elasticsearch/search/rank/rerank/RerankingRankCoordinatorContext.java
+++ server/src/main/java/org/elasticsearch/search/rank/rerank/RerankingRankCoordinatorContext.java
@@ -86,7 +86,7 @@ public abstract class RerankingRankCoordinatorContext extends RankCoordinatorCon
                 FeatureRankDoc[] sortedResults = rankMap.values().toArray(FeatureRankDoc[]::new);
                 Arrays.sort(sortedResults, Comparator.comparing((FeatureRankDoc doc) -> doc.score).reversed());
                 FeatureRankDoc[] topResults = new FeatureRankDoc[Math.min(size, sortedResults.length - from)];
-                for (int rank = 0; rank < topResults.length; ++rank) {
+                for (int rank = from; rank < topResults.length; ++rank) {
                     topResults[rank] = sortedResults[rank];
                     topResults[rank].rank = rank + 1 + from;
                 }
